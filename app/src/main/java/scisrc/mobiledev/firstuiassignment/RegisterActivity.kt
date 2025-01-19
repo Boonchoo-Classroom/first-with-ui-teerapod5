c324af1c0da21c49dbf74d649c415f7acb2157e8
Teerapod <Teerapod.p@ku.th>
2025-01-20 00:14:56 +0700

    Tong

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package scisrc.mobiledev.firstuiassignment
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 class RegisterActivity : AppCompatActivity() {
 
     lateinit var backToMainPageBtn: Button
+    lateinit var LogInTextView: TextView // เพิ่มตัวแปรสำหรับ TextView "Sign Up"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +25,14 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         backToMainPageBtn = findViewById(R.id.registBackToMainBtn)
+        LogInTextView = findViewById(R.id.textLogin)
 
         backToMainPageBtn.setOnClickListener() {
             finish()
+        }
+        LogInTextView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java) // สร้าง Intent
+            startActivity(intent) // เปิดหน้า RegisterActivity
         }
     }
 }
